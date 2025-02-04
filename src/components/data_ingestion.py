27cc2d973e3e1b085e5992594309e7f27f2663ae
Vipina7 <vipina1394@gmail.com>
2025-02-04 23:15:10 +0530

    exception.py update and train and test set ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,45 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+from dataclasses import dataclass
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path:str = os.path.join('artifacts', 'train.csv')
+    test_data_path:str = os.path.join('artifacts', 'test.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.data_ingestion_config = DataIngestionConfig()
+
+    def intiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+        try:
+            df = pd.read_csv('Notebook\Data\heart_disease_uci.csv')
+            logging.info('Read the dataset as dataframe')
+
+            os.makedirs(os.path.dirname(self.data_ingestion_config.train_data_path), exist_ok=True)
+            logging.info('Artifacts folder created')
+
+            logging.info("Train test split initiated")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(self.data_ingestion_config.train_data_path, index = False, header = True)
+            test_set.to_csv(self.data_ingestion_config.test_data_path, index = False, header = True)
+            logging.info('Ingestion of data is completed')
+
+            return(
+                self.data_ingestion_config.train_data_path,
+                self.data_ingestion_config.test_data_path
+            )
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
+if __name__ == "__main__":
+    data_obj = DataIngestion()
+    data_obj.intiate_data_ingestion()
